f3cb62f6f724d23e02b48a0e45349af7b233a525
shenxinrong <shenxinrong@weipaidai.com>
2017-08-16 17:58:23 +0800

    身份验证和银行卡列表

--- js/page/banklist.js
+++ js/page/banklist.js
@@ -1,25 +1,39 @@
 /**
- * 工作信息页
+ * 银行卡列表
  */
 ;
 (function() {
-	var kaiguan=true;
-	var kaiguan2=true;
+	var flag = 1;
+	var del=1;
+	var desc=1;
 	$('.gouxuan').on('click',function(){
-		kaiguan2 = false;
-		if(kaiguan){
+		
+		if(del == 1 && desc == 1){
 			$('.gouxuan').removeClass('activebank');
 			$(this).addClass('activebank');
-			kaiguan = false;
-		}else{
-			$('.gouxuan').removeClass('activebank');
-			$(this).addClass('activebank');
-			kaiguan = true;
-		}
-		if(!kaiguan2){
 			$('.page-button').addClass('activejie');
-		}else{
-			$('.page-button').removeClass('activejie');
+		}else if(del == 2 && desc == 2){
+			$('.tcdelz').css('display','block');
 		}
+		
+	})
+	$('.page-submit').on('click',function(){
+		desc =2;
+		if(del == 1){
+			$('.gouxuan').removeClass('activebank');
+			$('.gouxuan').addClass('delbank');
+			$('.page-button').removeClass('activejie');
+			del = 2;
+			desc =2;
+		}else if(del == 2){
+			$('.gouxuan').removeClass('activebank');
+			$('.gouxuan').removeClass('delbank');
+			$('.page-button').removeClass('activejie');
+			del = 1;
+			desc =1;
+		}
+	})
+	$('.hswz').on('click',function(){
+		$('.tcdelz').css('display','none');
 	})
 })();
